b60e0a8b4349f73d1f590d1a612684230450fc06
ksugimori <ksugimori.private@gmail.com>
2018-07-01 18:43:55 +0900

    replace request handlers with Servlet-like controllers.

---
+++ src/ksugimori/http/webapp/Router.java
@@ -0,0 +1,95 @@
+package ksugimori.http.webapp;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import ksugimori.http.webapp.controller.BasicHttpController;
+
+/**
+ * リクエストパスとコントローラの紐づけ
+ */
+public class Router {
+  /**
+   * コントローラを探索するパッケージ。サブパッケージは探索しません
+   */
+  public static final String CONTROLLER_PACKAGE_NAME = "ksugimori.http.webapp.controller";
+  
+  private static Router instance;
+  private Map<String, Controller> routingTable;
+
+  public static Router getInstance() {
+    if (instance == null) {
+      instance = new Router();
+    }
+    return instance;
+  }
+
+  private Router() {
+    instance = this;
+    routingTable = searchRoutedHandler();
+  }
+
+  /**
+   * コントローラの探索。{@link ksugimori.http.webapp.Router.CONTROLLER_PACKAGE_NAME}
+   * で指定されたパッケージのクラスで、{@link ksugimori.http.webapp.RequestMapping} アノテーションが付いたクラスを探し、リクエストパスと紐付ける。
+   * 
+   * @return
+   */
+  private Map<String, Controller> searchRoutedHandler() {
+    Map<String, Controller> table = new HashMap<>();
+
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    assert classLoader != null;
+    URL resource = classLoader.getResource(CONTROLLER_PACKAGE_NAME.replace('.', '/'));
+
+    Path packageDir;
+    try {
+      packageDir = Paths.get(resource.toURI());
+    } catch (URISyntaxException e) {
+      return Collections.emptyMap();
+    }
+    
+    try (DirectoryStream<Path> filespaths = Files.newDirectoryStream(packageDir, "*.class")) {
+      for (Path filepath : filespaths) {
+        String simpleName = filepath.getFileName().toString().replaceAll("\\.class$", "");
+        String className = CONTROLLER_PACKAGE_NAME + "." + simpleName;
+        try {
+          Class<?> clazz = Class.forName(className);
+          if ( ! clazz.isAnnotationPresent(RequestMapping.class)) continue;
+
+          String path = clazz.getDeclaredAnnotation(RequestMapping.class).value();
+          Controller controller = (Controller) clazz.newInstance();
+          table.put(path, controller);
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+          // do nothing
+        }
+      }
+    } catch (IOException e) {
+      System.err.println("fail loading routed classes.");
+    }
+
+    return table;
+  }
+
+  /**
+   * ルーティングが設定されていればそのハンドラ、なければデフォルトのハンドラを返す
+   * 
+   * @param path
+   * @return
+   */
+  public Controller route(String path) {
+    String normalized = Paths.get(path).normalize().toString();
+    if (routingTable.containsKey(normalized)) {
+      return routingTable.get(normalized);
+    } else {
+      return new BasicHttpController();
+    }
+  }
+}
--- src/ksugimori/http/webapp/controller/BasicHttpController.java
+++ src/ksugimori/http/webapp/controller/BasicHttpController.java
@@ -1,6 +1,7 @@
-package ksugimori.http.handler;
+package ksugimori.http.webapp.controller;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,15 +10,16 @@ import ksugimori.http.message.Response;
 import ksugimori.http.message.Status;
 import ksugimori.http.server.SimpleHttpServer;
 import ksugimori.http.util.StringUtils;
+import ksugimori.http.webapp.Controller;
 
 /**
- * GET リクエストのハンドラ
+ * 通常の HTTP リクエストを処理するコントローラ
  */
-public class GetRequestHandler implements RequestHandler {
+public class BasicHttpController extends Controller {
   public static String protocolVersion = "HTTP/1.1";
 
   @Override
-  public Response handle(Request request) {
+  public Response doGet(Request request) {
     Path target = Paths.get(SimpleHttpServer.getDocumentRoot(), request.getTarget()).normalize();
 
     // ドキュメントルート以下のみアクセス可能にする
@@ -33,10 +35,10 @@ public class GetRequestHandler implements RequestHandler {
     try {
       response = new Response(protocolVersion, Status.OK);
       response.setBody(Files.readAllBytes(target));
-      
+
       String ext = StringUtils.getFileExtension(target.getFileName().toString());
       String contentType = SimpleHttpServer.extensionToContentType(ext);
-      
+
       response.addHeaderField("Content-Type", contentType);
     } catch (IOException e) {
       response = new Response(protocolVersion, Status.NOT_FOUND);
@@ -46,4 +48,11 @@ public class GetRequestHandler implements RequestHandler {
     return response;
   }
 
+  @Override
+  public Response doPost(Request request) {
+    // nothing to do
+    System.out.println("POST body: " + new String(request.getBody(), StandardCharsets.UTF_8));
+    Response response = new Response(protocolVersion, Status.NO_CONTENT);
+    return response;
+  }
 }
